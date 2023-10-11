b2140af3c9f0dc816891fe8f88f43d24ac8c4857
paila.hemanth <pailahemanth2002@gmail.com>
2023-10-11 15:42:57 +0530

    added skills section

--- src/App.js
+++ src/App.js
@@ -26,6 +26,24 @@ function App() {
         <div className='home_page_designation'>&nbsp;software development engineer</div>
       </div>
     </div>
+    <div className='about_container'>
+      <div className='about_heading'>ABOUT</div>
+      <div className='content_container'>
+        <img src="/images/logo_bee.png" alt="error" />
+        <div className='about_content'>I'm Hemanth Paila, a MERN Stack Developer and Software Engineer who thrives in crafting efficient web 
+        applications, mirroring the harmony of a beehive.My expertise spans both frontend and backend development, ensuring seamless integration 
+        and innovation.I meticulously build code, just as bees craft hexagonal cells, focusing on robustness and scalability.My goal is to elevate 
+        businesses, 
+        propelling them to new horizons, much like bees take to the sky.I thrive in team environments, understanding that unity, like in a hive, is 
+        the source of strength. In tech's ever-evolving landscape, I embrace constant learning, expanding my knowledge and skills.I'm a debugging 
+        dynamo, committed to ensuring optimal performance in every project.Let's collaborate to create digital landscapes as productive as a 
+        thriving bee colony. </div>
+      </div>
+      <div className='skills_container'>
+        <div className='skills_heading'>SKILLS</div>
+        <img src="/images/skills1.jpg" alt="" />
+      </div>
+    </div>
     </div>
   );
 }
